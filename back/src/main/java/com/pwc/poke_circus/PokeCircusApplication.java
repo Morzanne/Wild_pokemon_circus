e250c8720ab87213bfce897a2c53175ff4e33e86
Aurélien Grenet <aurel.grenet@gmail.com>
2019-07-16 12:50:57 +0200

    init-back

---
+++ back/src/main/java/com/pwc/poke_circus/PokeCircusApplication.java
@@ -0,0 +1,13 @@
+package com.pwc.poke_circus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeCircusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeCircusApplication.class, args);
+	}
+
+}
